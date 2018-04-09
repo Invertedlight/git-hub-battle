9b56dc49e78c0243c57c9f5f0b2db470564800b4
James Richardson <James.Richardson@invertedlight.com>
2018-04-08 23:00:32 -0500

    Refactoring to remove axios in favor of fetch

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var webpack = require('webpack');
 
 var config = {
 	mode: "development",
-	entry: ['babel-polyfill', './app/index.js',],
+	entry: ['babel-polyfill', 'whatwg-fetch', './app/index.js',],
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'index_bundle.js',
